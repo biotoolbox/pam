630f52fe1bba29b8e974771aa32fab0ebe13456d
Phi-S <926151+Phi-S@users.noreply.github.com>
2024-09-17 23:59:21 +0200

    walsby + platt + wrapper

--- R/hello.R
+++ R/hello.R
@@ -155,6 +155,21 @@ plot_control_raw <- function(data, title, use_etr_I) {
   return(plot)
 }
 
+calculate_sdiff <- function(data, etr_regression_data, etr_to_use) {
+  finalSdiff <- 0
+  for (i in seq_len(nrow(data))) {
+    row <- data[i]
+    realEtr <- row[[etr_to_use]]
+    predictedEtr <- etr_regression_data[PAR == row$PAR, prediction]
+    sdiff <- (predictedEtr - realEtr)^2
+    finalSdiff <- finalSdiff + sdiff
+  }
+
+  return(finalSdiff)
+}
+
+####################
+
 generate_regression_eilers_peeters <- function(data, use_etr_I) {
   library(minpack.lm)
   validate_data(data)
@@ -172,7 +187,7 @@ generate_regression_eilers_peeters <- function(data, use_etr_I) {
     data <- data[Action != "Pm.-Det."]
   }
 
-  model <- nls(data[[etr_to_use]] ~ (PAR / (a * PAR^2 + b * PAR + c)),
+  model <- nlsLM(data[[etr_to_use]] ~ (PAR / (a * PAR^2 + b * PAR + c)),
     data = data,
     start = list(a = 0.00004, b = 0.004, c = 5)
   ) # Initial parameter values
@@ -181,6 +196,7 @@ generate_regression_eilers_peeters <- function(data, use_etr_I) {
   a <- abc[["a"]]
   b <- abc[["b"]]
   c <- abc[["c"]]
+  
   etr_max <- 1 / (b + 2 * sqrt(a * c))
 
   # Calculate alpha using the formula
@@ -192,8 +208,25 @@ generate_regression_eilers_peeters <- function(data, use_etr_I) {
   # Calculate Im using the formula
   Im <- sqrt(c / a)
 
+  pars <- c()
+  predictions <- c()
+  for (p in min(data$PAR):max(data$PAR)) {
+    pars <- c(pars, p)
+    predictions <- c(predictions, p / ((a * p^2) + (b * p) + c))
+  }
+  etr_regression_data <- data.table("PAR" = pars, "prediction" = predictions)
 
-  return(c(a = a, b = b, c = c, etr_max = etr_max, alpha = alpha, ik = Ik, im = Im))
+  return(list(
+    etr_regression_data = etr_regression_data,
+    sdiff = calculate_sdiff(data, etr_regression_data, etr_to_use),
+    a = a,
+    b = b,
+    c = c,
+    etr_max = etr_max,
+    alpha = alpha,
+    ik = Ik,
+    im = Im
+  ))
 }
 
 plot_control_eilers_peeters <- function(data, regression_data, title, use_etr_I) {
@@ -209,14 +242,7 @@ plot_control_eilers_peeters <- function(data, regression_data, title, use_etr_I)
   a <- eval(regression_data[["a"]])
   b <- eval(regression_data[["b"]])
   c <- eval(regression_data[["c"]])
-
-  pars <- c()
-  predictions <- c()
-  for (p in min(data$PAR):max(data$PAR)) {
-    pars <- c(pars, p)
-    predictions <- c(predictions, p / ((a * p^2) + (b * p) + c))
-  }
-  line_data <- data.table("PAR" = pars, "prediction" = predictions)
+  etr_regression_data <- eval(regression_data[["etr_regression_data"]])
 
   etr_to_use <- ""
   if (use_etr_I) {
@@ -230,13 +256,223 @@ plot_control_eilers_peeters <- function(data, regression_data, title, use_etr_I)
   # Create plot for ETR.II. by PAR and filename
   plot <- ggplot(data, aes(x = PAR, y = get(etr_to_use))) +
     geom_point() +
-    geom_line(data = line_data, aes(x = PAR, y = prediction), color = "#f700ff") +
+    geom_line(data = etr_regression_data, aes(x = PAR, y = prediction), color = "#f700ff") +
     labs(x = par_label, y = etr_label, title = eval(title)) +
     theme_minimal() +
     labs(caption = glue("ETRmax: {round(regression_data[['etr_max']], 3)}
     alpha: {round(regression_data[['alpha']], 3)}
     Ik: {round(regression_data[['ik']], 3)}
-    Im: {round(regression_data[['im']], 3)}")) +
+    Im: {round(regression_data[['im']], 3)}
+    SDiff: {round(regression_data[['sdiff']], 3)}")) +
+    theme(plot.caption = element_text(hjust = 0))
+
+  return(plot)
+}
+
+generate_regression_walsby <- function(data, use_etr_I) {
+  library(minpack.lm)
+  library(SciViews)
+  validate_data(data)
+
+  if (!is.logical(use_etr_I)) {
+    stop("use_etr_I is not a valid bool")
+  }
+
+  etr_to_use <- ""
+  if (use_etr_I) {
+    etr_to_use <- etr_I_col_name
+    data <- data[Action != "Fm-Det."]
+  } else {
+    etr_to_use <- etr_II_col_name
+    data <- data[Action != "Pm.-Det."]
+  }
+
+  model <- nlsLM(data[[etr_to_use]] ~ a * (1 - exp((-b * PAR) / a)) + c * PAR,
+    data = data,
+    start = list(a = 100, b = 0.4, c = -0.01)
+  )
+
+  abc <- coef(model)
+  a <- abc[["a"]]
+  b <- abc[["b"]]
+  c <- abc[["c"]]
+
+  # Calculate Iopt using the formula
+  Iopt <- -(a * ln(-c / b)) / b
+
+  # Calculate ETRmax using the formula
+  ETRmax <- a * (1 - exp(-b / a * Iopt)) + c * Iopt
+
+  # Calculate alpha using the formula
+  alpha <- b
+
+  # Calculate Ik using the formula
+  Ik <- a / b
+
+  # Calculate beta using the formula
+  beta <- c
+
+  pars <- c()
+  predictions <- c()
+  for (p in min(data$PAR):max(data$PAR)) {
+    pars <- c(pars, p)
+    predictions <- c(predictions, a * (1 - exp((-b * p) / a)) + c * p)
+  }
+  etr_regression_data <- data.table("PAR" = pars, "prediction" = predictions)
+
+  return(list(
+    etr_regression_data = etr_regression_data,
+    sdiff = calculate_sdiff(data, etr_regression_data, etr_to_use),
+    a = a,
+    b = b,
+    c = c,
+    iopt = Iopt,
+    etr_max = ETRmax,
+    alpha = alpha,
+    ik = Ik,
+    beta = beta
+  ))
+}
+
+plot_control_walsby <- function(data, regression_data, title, use_etr_I) {
+  library(ggplot2)
+  library(glue)
+
+  validate_data(data)
+
+  if (!is.logical(use_etr_I)) {
+    stop("use_etr_I is not a valid bool")
+  }
+
+  a <- eval(regression_data[["a"]])
+  b <- eval(regression_data[["b"]])
+  c <- eval(regression_data[["c"]])
+  etr_regression_data <- regression_data[["etr_regression_data"]]
+
+  etr_to_use <- ""
+  if (use_etr_I) {
+    etr_to_use <- etr_I_col_name
+    data <- data[Action != "Fm-Det."]
+  } else {
+    etr_to_use <- etr_II_col_name
+    data <- data[Action != "Pm.-Det."]
+  }
+
+  # Create plot for ETR.II. by PAR and filename
+  plot <- ggplot(data, aes(x = PAR, y = get(etr_to_use))) +
+    geom_point() +
+    geom_line(data = etr_regression_data, aes(x = PAR, y = prediction), color = "#f700ff") +
+    labs(x = par_label, y = etr_label, title = eval(title)) +
+    theme_minimal() +
+    labs(caption = glue("ETRmax: {round(regression_data[['etr_max']], 3)}
+    alpha: {round(regression_data[['alpha']], 3)}
+    Ik: {round(regression_data[['ik']], 3)}
+    beta: {round(regression_data[['beta']], 3)}
+    Iopt: {round(regression_data[['iopt']], 3)}
+    SDiff: {round(regression_data[['sdiff']], 3)}")) +
+    theme(plot.caption = element_text(hjust = 0))
+
+  return(plot)
+}
+
+generate_regression_platt <- function(data, use_etr_I) {
+  library(minpack.lm)
+  library(SciViews)
+  validate_data(data)
+
+  if (!is.logical(use_etr_I)) {
+    stop("use_etr_I is not a valid bool")
+  }
+
+  etr_to_use <- ""
+  if (use_etr_I) {
+    etr_to_use <- etr_I_col_name
+    data <- data[Action != "Fm-Det."]
+  } else {
+    etr_to_use <- etr_II_col_name
+    data <- data[Action != "Pm.-Det."]
+  }
+
+  model <- nlsLM(data[[etr_to_use]] ~ ETRmPot * (1 - exp((-a * PAR) / ETRmPot)) * exp((-b * PAR) / ETRmPot),
+    data = data,
+    start = list(a = 0.3, b = 0.01, ETRmPot = 30)
+  )
+
+  abc <- coef(model)
+  a <- abc[["a"]]
+  b <- abc[["b"]]
+  ETRmPot <- abc[["ETRmPot"]]
+
+  # Calculate ETRmax using the formula
+  ETRmax <- ETRmPot * (a / (a + b)) * ((b / (a + b))^(b / a))
+
+  # Calculate alpha using the formula
+  alpha <- a
+
+  # Calculate Ik using the formula
+  Ik <- ETRmax / a
+
+  # Calculate beta using the formula
+  beta <- b
+
+  pars <- c()
+  predictions <- c()
+  for (p in min(data$PAR):max(data$PAR)) {
+    pars <- c(pars, p)
+    predictions <- c(predictions, ETRmPot * (1 - exp((-a * p) / ETRmPot)) * exp((-b * p) / ETRmPot))
+  }
+  etr_regression_data <- data.table("PAR" = pars, "prediction" = predictions)
+
+  return(list(
+    sdiff = calculate_sdiff(data, etr_regression_data, etr_to_use),
+    etr_regression_data = etr_regression_data,
+    a = a,
+    b = b,
+    ETRmPot = ETRmPot,
+    etr_max = ETRmax,
+    alpha = alpha,
+    ik = Ik,
+    beta = beta
+  ))
+}
+
+plot_control_platt <- function(data, regression_data, title, use_etr_I) {
+  library(ggplot2)
+  library(glue)
+
+  validate_data(data)
+  # TODO validate regression data
+
+  if (!is.logical(use_etr_I)) {
+    stop("use_etr_I is not a valid bool")
+  }
+
+  sdiff <- eval(regression_data[["sdiff"]])
+  etr_regression_data <- eval(regression_data[["etr_regression_data"]])
+  a <- eval(regression_data[["a"]])
+  b <- eval(regression_data[["b"]])
+  ETRmPot <- eval(regression_data[["ETRmPot"]])
+
+  etr_to_use <- ""
+  if (use_etr_I) {
+    etr_to_use <- etr_I_col_name
+    data <- data[Action != "Fm-Det."]
+  } else {
+    etr_to_use <- etr_II_col_name
+    data <- data[Action != "Pm.-Det."]
+  }
+
+  # Create plot for ETR.II. by PAR and filename
+  plot <- ggplot(data, aes(x = PAR, y = get(etr_to_use))) +
+    geom_point() +
+    geom_line(data = etr_regression_data, aes(x = PAR, y = prediction), color = "#f700ff") +
+    labs(x = par_label, y = etr_label, title = eval(title)) +
+    theme_minimal() +
+    labs(caption = glue("ETRmax: {round(regression_data[['etr_max']], 3)}
+    alpha: {round(regression_data[['alpha']], 3)}
+    Ik: {round(regression_data[['ik']], 3)}
+    beta: {round(regression_data[['beta']], 3)}
+    SDiff: {round(regression_data[['sdiff']], 3)}")) +
     theme(plot.caption = element_text(hjust = 0))
 
   return(plot)
--- tests/method_test.R
+++ tests/method_test.R
@@ -19,18 +19,57 @@
 
 library(glue)
 csv_files <- list.files(file.path(getwd(), "data"), pattern = ".csv", full.names = TRUE)
-
-# pdf("plots.pdf", onefile = TRUE)
-# Iterate over each .csv file
 for (file in csv_files) {
   title <- basename(file)
-  png(glue("{title}.png"))
   data <- read_pam_data(file)
-  # View(data)
-  # print(plot_control_raw(data, basename(file), TRUE))
-  reg_data <- generate_regression_eilers_peeters(data, FALSE)
-  View(reg_data)
-  print(plot_control_eilers_peeters(data, reg_data, title, FALSE))
+  png(glue("{title}_combo_plot.png"))
+  print(plot_combo(title, data, TRUE))
   dev.off()
-  stop("a")
+  # stop("a")
+}
+
+# print("asdfg")
+# View(create_regression_data_table_eilers_peeters(csv_files, FALSE))
+# write.csv(create_regression_data_table_eilers_peeters(csv_files, FALSE), "keke.csv")
+
+test1 <- function(files) {
+  # pdf("plots.pdf", onefile = TRUE)
+  # Iterate over each .csv file
+  for (file in csv_files) {
+    title <- basename(file)
+    data <- read_pam_data(file)
+    # View(data)
+    # print(plot_control_raw(data, basename(file), TRUE))
+
+    reg_data_eilers_peeters <- generate_regression_eilers_peeters(data, FALSE)
+    print("reg_data eilers_peeters: ")
+    print(reg_data_eilers_peeters)
+    View(reg_data_eilers_peeters)
+
+    reg_data_walsby <- generate_regression_walsby(data, FALSE)
+    print("reg_data_walsby: ")
+    print(reg_data_walsby)
+
+    reg_data_platt <- generate_regression_platt(data, FALSE)
+    print("reg_data_platt: ")
+    print(reg_data_platt)
+
+    # View(create_regression_data_table(reg_data_eilers_peeters, reg_data_walsby, reg_data_platt))
+
+    print(min(c(reg_data_eilers_peeters$sdiff, reg_data_walsby$sdiff, reg_data_platt$sdiff)))
+
+    png(glue("{title}_eilers_peeters.png"))
+    print(plot_control_eilers_peeters(data, reg_data_eilers_peeters, title, FALSE))
+    dev.off()
+
+    png(glue("{title}_walsby.png"))
+    print(plot_control_walsby(data, reg_data_walsby, title, FALSE))
+    dev.off()
+
+    png(glue("{title}_platt.png"))
+    print(plot_control_platt(data, reg_data_platt, title, FALSE))
+    dev.off()
+
+    stop("a")
+  }
 }
