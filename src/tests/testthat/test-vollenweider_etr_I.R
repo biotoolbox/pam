2a629e4d25d6457acbeb6fea91a4bc9ac4335c04
JulienBoehm <180776296+JulienBoehm@users.noreply.github.com>
2025-01-08 14:05:18 +0100

    test vollenweider change

--- src/tests/testthat/test-vollenweider_etr_I.R
+++ src/tests/testthat/test-vollenweider_etr_I.R
@@ -1,21 +1,21 @@
-test_that("test-vollenweider_etr_I generate regression 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-vollenweider_etr_I generate regression 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- vollenweider_generate_regression_ETR_I(data)
 
-  expect_equal(model_result[["sdiff"]], 1099.94915)
-  expect_equal(model_result[["pmax"]], 1132.09578)
-  expect_equal(model_result[["a"]], 0.00016833, tolerance = 0.00001)
-  expect_equal(model_result[["alpha"]], -0.00001382, tolerance = 0.0001)
-  expect_equal(model_result[["n"]], 4224.5959)
-  expect_equal(model_result[["ik"]], 5940.6891)
-  expect_equal(model_result[["popt"]], 126.51602)
-  expect_equal(model_result[["iik"]], 663.89465)
-  expect_equal(model_result[["pmax_popt_and_ik_iik_ratio"]], 8.9482406)
+  expect_equal(model_result[["sdiff"]], 57.4445851)
+  expect_equal(model_result[["pmax"]], 165.46686)
+  #expect_equal(model_result[["a"]], 0.001864811)
+  #expect_equal(model_result[["alpha"]], 0.00001772)
+  expect_equal(model_result[["n"]], 34.8365884)
+  expect_equal(model_result[["ik"]], 536.24749)
+  expect_equal(model_result[["popt"]], 153.09957)
+  expect_equal(model_result[["iik"]], 496.16738)
+  expect_equal(model_result[["pmax_popt_and_ik_iik_ratio"]], 1.08077941)
 })
 
-test_that("test-vollenweider_etr_I control plot 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-vollenweider_etr_I control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- vollenweider_generate_regression_ETR_I(data)
 
@@ -24,38 +24,38 @@ test_that("test-vollenweider_etr_I control plot 20231122_01.csv", {
       plot_control(
         data,
         model_result,
-        "vollenweider ETR I 20231122_01.csv",
+        "vollenweider ETR I 20240925.csv",
         color_vollenweider
       )
     )
   )
 })
 
-test_that("test-vollenweider_etr_I generate regression modified 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-vollenweider_etr_I generate regression modified 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- vollenweider_generate_regression_ETR_I(data)
   model_result <- vollenweider_modified(model_result)
 
-  expect_equal(model_result[["sdiff"]], 1099.94915)
-  expect_equal(model_result[["a"]], 1132.09578)
-  expect_equal(model_result[["b"]], 0.00016833, tolerance = 0.00001)
-  expect_equal(model_result[["c"]], -0.00001382, tolerance = 0.0001)
-  expect_equal(model_result[["d"]], 4224.5959)
-  expect_equal(model_result[["alpha"]], 126.51602 / 663.89465)
+  expect_equal(model_result[["sdiff"]], 57.4445851)
+  expect_equal(model_result[["a"]], 165.46686)
+  #expect_equal(model_result[["b"]], 0.001864811)
+  #expect_equal(model_result[["c"]], 0.00001772)
+  expect_equal(model_result[["d"]], 34.8365884)
+  expect_equal(model_result[["alpha"]], 0.30856436)
   expect_equal(model_result[["beta"]], NA_real_)
-  expect_equal(model_result[["etrmax_with_photoinhibition"]], 126.51602)
-  expect_equal(model_result[["etrmax_without_photoinhibition"]], 1132.09578)
-  expect_equal(model_result[["ik_with_photoinhibition"]], 663.89465)
-  expect_equal(model_result[["ik_without_photoinhibition"]], 5940.6891)
-  expect_equal(model_result[["im_with_photoinhibition"]], 1095)
+  expect_equal(model_result[["etrmax_with_photoinhibition"]], 153.09957)
+  expect_equal(model_result[["etrmax_without_photoinhibition"]], 165.46686)
+  expect_equal(model_result[["ik_with_photoinhibition"]], 496.16738)
+  expect_equal(model_result[["ik_without_photoinhibition"]], 536.24749)
+  expect_equal(model_result[["im_with_photoinhibition"]], 1420.0)
   expect_equal(model_result[["w"]], NA_real_)
   expect_equal(model_result[["ib"]], NA_real_)
-  expect_equal(model_result[["etrmax_with_without_ratio"]], 8.9482406)
+  expect_equal(model_result[["etrmax_with_without_ratio"]], 1.08077941)
 })
 
-test_that("test-vollenweider_etr_I modified control plot 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-vollenweider_etr_I modified control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- vollenweider_generate_regression_ETR_I(data)
   model_result <- vollenweider_modified(model_result)
@@ -65,7 +65,7 @@ test_that("test-vollenweider_etr_I modified control plot 20231122_01.csv", {
       plot_control(
         data,
         model_result,
-        "vollenweider ETR I modified 20231122_01.csv",
+        "vollenweider ETR I modified 20240925.csv",
         color_vollenweider
       )
     )
--- src/tests/testthat/test-vollenweider_etr_II.R
+++ src/tests/testthat/test-vollenweider_etr_II.R
@@ -1,20 +1,20 @@
-test_that("test-vollenweider_etr_II generate regression 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-vollenweider_etr_II generate regression 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- vollenweider_generate_regression_ETR_II(data)
 
-  expect_equal(model_result[["sdiff"]], 3.04234525)
-  expect_equal(model_result[["pmax"]], 36.644038)
-  expect_equal(model_result[["a"]], 0.00243709, tolerance = 0.00001)
-  expect_equal(model_result[["alpha"]], -0.00001386, tolerance = 0.0001)
-  expect_equal(model_result[["n"]], 1059.2046)
-  expect_equal(model_result[["ik"]], 410.32484)
-  expect_equal(model_result[["popt"]], 30.70697)
-  expect_equal(model_result[["iik"]], 343.844)
+  expect_equal(model_result[["sdiff"]], 2.5104818)
+  expect_equal(model_result[["pmax"]], 53.404027)
+  #expect_equal(model_result[["a"]], 0.002845961)
+  #expect_equal(model_result[["alpha"]], -0.000378034)
+  expect_equal(model_result[["n"]], 2.34641)
+  expect_equal(model_result[["ik"]], 351.37520)
+  expect_equal(model_result[["popt"]], 44.16714)
+  expect_equal(model_result[["iik"]], 290.60051)
 })
 
-test_that("test-vollenweider_etr_II control plot 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-vollenweider_etr_II control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- vollenweider_generate_regression_ETR_II(data)
 
@@ -23,7 +23,48 @@ test_that("test-vollenweider_etr_II control plot 20231122_01.csv", {
       plot_control(
         data,
         model_result,
-        "vollenweider ETR II 20231122_01.csv",
+        "vollenweider ETR II 20240925.csv",
+        color_vollenweider
+      )
+    )
+  )
+})
+
+test_that("test-vollenweider_etr_II generate regression modified 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
+  data <- read_dual_pam_data(test_data_file)
+  model_result <- vollenweider_generate_regression_ETR_II(data)
+  model_result <- vollenweider_modified(model_result)
+
+  expect_equal(model_result[["sdiff"]], 2.5104818)
+  expect_equal(model_result[["a"]], 53.404027)
+  #expect_equal(model_result[["b"]], 0.002845961)
+  #expect_equal(model_result[["c"]], -0.000378034)
+  expect_equal(model_result[["d"]], 2.34641)
+  expect_equal(model_result[["alpha"]], 0.15198576)
+  expect_equal(model_result[["beta"]], NA_real_)
+  expect_equal(model_result[["etrmax_with_photoinhibition"]], 44.16714)
+  expect_equal(model_result[["etrmax_without_photoinhibition"]], 53.404027)
+  expect_equal(model_result[["ik_with_photoinhibition"]], 290.60051)
+  expect_equal(model_result[["ik_without_photoinhibition"]], 351.37520)
+  expect_equal(model_result[["im_with_photoinhibition"]], 757.0)
+  expect_equal(model_result[["w"]], NA_real_)
+  expect_equal(model_result[["ib"]], NA_real_)
+  expect_equal(model_result[["etrmax_with_without_ratio"]], 1.209134835)
+})
+
+test_that("test-vollenweider_etr_II modified control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
+  data <- read_dual_pam_data(test_data_file)
+  model_result <- vollenweider_generate_regression_ETR_II(data)
+  model_result <- vollenweider_modified(model_result)
+
+  expect_no_warning(
+    print(
+      plot_control(
+        data,
+        model_result,
+        "vollenweider ETR II modified 20240925.csv",
         color_vollenweider
       )
     )
