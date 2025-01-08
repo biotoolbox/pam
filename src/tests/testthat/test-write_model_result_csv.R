8ff8dc2e30571253ef9fb2c7cdb2b4148979a9b9
JulienBoehm <180776296+JulienBoehm@users.noreply.github.com>
2025-01-08 16:47:56 +0100

    test combo plot change

--- src/tests/testthat/test-write_model_result_csv.R
+++ src/tests/testthat/test-write_model_result_csv.R
@@ -1,5 +1,5 @@
-test_that("test-write_model_result_csv 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-write_model_result_csv 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- walsby_generate_regression_ETR_II(data)
   model_result <- walsby_modified(model_result)
@@ -10,7 +10,7 @@ test_that("test-write_model_result_csv 20231122_01.csv", {
   expect_no_warning(
     write_model_result_csv(
       result_dir,
-      "20231122_01_W3_T20_HL_ETR_II",
+      "20240925",
       data,
       model_result
     )
