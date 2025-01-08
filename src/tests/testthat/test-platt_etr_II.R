0c8657d90fc511190f4eb9779755175af0e75d44
JulienBoehm <180776296+JulienBoehm@users.noreply.github.com>
2025-01-08 12:30:22 +0100

    test platt change

--- src/tests/testthat/test-platt_etr_II.R
+++ src/tests/testthat/test-platt_etr_II.R
@@ -1,21 +1,21 @@
-test_that("test-platt_etr_II generate regression 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-platt_etr_II generate regression 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- platt_generate_regression_ETR_II(data)
 
-  expect_equal(model_result[["sdiff"]], 4.4844798)
-  expect_equal(model_result[["alpha"]], 0.1028022)
-  expect_equal(model_result[["beta"]], 0.02631246, tolerance = 0.000001)
-  expect_equal(model_result[["ps"]], 58.592426)
-  expect_equal(model_result[["pm"]], 31.049374)
-  expect_equal(model_result[["ik"]], 302.03024)
-  expect_equal(model_result[["is"]], 569.95303)
-  expect_equal(model_result[["ib"]], 2226.79356)
-  expect_equal(model_result[["im"]], 906.60042)
+  expect_equal(model_result[["sdiff"]], 12.0348133)
+  expect_equal(model_result[["alpha"]], 0.178182825)
+  expect_equal(model_result[["beta"]], 0.035606479)
+  expect_equal(model_result[["ps"]], 76.53219)
+  expect_equal(model_result[["pm"]], 44.5824637)
+  expect_equal(model_result[["ik"]], 250.2062903)
+  expect_equal(model_result[["is"]], 429.514971)
+  expect_equal(model_result[["ib"]], 2149.38944)
+  expect_equal(model_result[["im"]], 769.88984)
 })
 
-test_that("test-platt_etr_II control plot 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-platt_etr_II control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- platt_generate_regression_ETR_II(data)
 
@@ -24,38 +24,38 @@ test_that("test-platt_etr_II control plot 20231122_01.csv", {
       plot_control(
         data,
         model_result,
-        "platt ETR II 20231122_01.csv",
+        "platt ETR II 20240925.csv",
         color_platt
       )
     )
   )
 })
 
-test_that("test-platt_etr_II generate regression modified 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-platt_etr_II generate regression modified 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- platt_generate_regression_ETR_II(data)
   model_result <- platt_modified(model_result)
 
-  expect_equal(model_result[["sdiff"]], 4.4844798)
-  expect_equal(model_result[["a"]], 58.592426)
-  expect_equal(model_result[["b"]], 0.1028022)
-  expect_equal(model_result[["c"]], 0.02631246, tolerance = 0.000001)
+  expect_equal(model_result[["sdiff"]], 12.0348133)
+  expect_equal(model_result[["a"]], 76.53219)
+  expect_equal(model_result[["b"]], 0.178182825)
+  expect_equal(model_result[["c"]], 0.035606479)
   expect_equal(model_result[["d"]], NA_real_)
-  expect_equal(model_result[["alpha"]], 0.1028022)
-  expect_equal(model_result[["beta"]], 0.02631246, tolerance = 0.000001)
-  expect_equal(model_result[["etrmax_with_photoinhibition"]], 31.049374)
-  expect_equal(model_result[["etrmax_without_photoinhibition"]], 58.592426)
-  expect_equal(model_result[["ik_with_photoinhibition"]], 302.03024)
-  expect_equal(model_result[["ik_without_photoinhibition"]], 569.95303)
-  expect_equal(model_result[["im_with_photoinhibition"]], 906.60042)
+  expect_equal(model_result[["alpha"]], 0.178182825)
+  expect_equal(model_result[["beta"]], 0.035606479)
+  expect_equal(model_result[["etrmax_with_photoinhibition"]], 44.5824637)
+  expect_equal(model_result[["etrmax_without_photoinhibition"]], 76.53219)
+  expect_equal(model_result[["ik_with_photoinhibition"]], 250.2062903)
+  expect_equal(model_result[["ik_without_photoinhibition"]], 429.514971)
+  expect_equal(model_result[["im_with_photoinhibition"]], 769.88984)
   expect_equal(model_result[["w"]], NA_real_)
-  expect_equal(model_result[["ib"]], 2226.79356)
-  expect_equal(model_result[["etrmax_with_without_ratio"]], 58.592426 / 31.049374)
+  expect_equal(model_result[["ib"]], 2149.38944)
+  expect_equal(model_result[["etrmax_with_without_ratio"]], 1.71664337)
 })
 
-test_that("test-platt_etr_II modified control plot 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-platt_etr_II modified control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- platt_generate_regression_ETR_II(data)
   model_result <- platt_modified(model_result)
@@ -65,7 +65,7 @@ test_that("test-platt_etr_II modified control plot 20231122_01.csv", {
       plot_control(
         data,
         model_result,
-        "platt ETR II modified 20231122_01.csv",
+        "platt ETR II modified 20240925.csv",
         color_platt
       )
     )
