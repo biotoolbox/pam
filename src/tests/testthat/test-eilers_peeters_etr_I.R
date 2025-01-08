a7ff2aaceca545f1561446e07aba674f92276ecd
JulienBoehm <180776296+JulienBoehm@users.noreply.github.com>
2025-01-08 13:23:24 +0100

    test eilers and peeters update

--- src/tests/testthat/test-eilers_peeters_etr_I.R
+++ src/tests/testthat/test-eilers_peeters_etr_I.R
@@ -1,21 +1,21 @@
-test_that("test-eilers_peeters_etr_I generate regression 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-eilers_peeters_etr_I generate regression 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- eilers_peeters_generate_regression_ETR_I(data)
 
-  expect_equal(model_result[["sdiff"]], 1183.4214)
-  # expect_equal(model_result[["a"]], 0.0000062986, tolerance = 0.0001)
-  expect_equal(model_result[["b"]], -0.00523754, tolerance = 0.00001)
-  expect_equal(model_result[["c"]], 6.8374816)
-  expect_equal(model_result[["pm"]], 126.78253)
-  expect_equal(model_result[["s"]], 0.14625268)
-  expect_equal(model_result[["ik"]], 866.87322)
-  expect_equal(model_result[["im"]], 1041.89685)
-  expect_equal(model_result[["w"]], -0.79809778)
+  expect_equal(model_result[["sdiff"]], 53.4454062)
+  #expect_equal(model_result[["a"]], 0.000001479)
+  #expect_equal(model_result[["b"]], 0.002444096)
+  expect_equal(model_result[["c"]], 2.8984079)
+  expect_equal(model_result[["pm"]], 151.851986)
+  expect_equal(model_result[["s"]], 0.345017)
+  expect_equal(model_result[["ik"]], 440.1289906)
+  expect_equal(model_result[["im"]], 1399.76963)
+  expect_equal(model_result[["w"]], 1.180362267)
 })
 
-test_that("test-eilers_peeters_etr_I control plot 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-eilers_peeters_etr_I control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- eilers_peeters_generate_regression_ETR_I(data)
 
@@ -24,40 +24,40 @@ test_that("test-eilers_peeters_etr_I control plot 20231122_01.csv", {
       plot_control(
         data,
         model_result,
-        "eilers_peeters ETR I 20231122_01.csv",
+        "eilers_peeters ETR I 20240925.csv",
         color_eilers_peeters
       )
     )
   )
 })
 
-test_that("test-eilers_peeters_etr_I generate regression modified 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-eilers_peeters_etr_I generate regression modified 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- eilers_peeters_generate_regression_ETR_I(data)
   expect_no_warning(
     model_result <- eilers_peeters_modified(model_result)
   )
 
-  expect_equal(model_result[["sdiff"]], 1183.4214)
-  # expect_equal(model_result[["a"]], 0.0000062986, tolerance = 0.0001)
-  expect_equal(model_result[["b"]], -0.00523754, tolerance = 0.00001)
-  expect_equal(model_result[["c"]], 6.8374816)
+  expect_equal(model_result[["sdiff"]], 53.4454062)
+  #expect_equal(model_result[["a"]], 0.000001479)
+  #expect_equal(model_result[["b"]], 0.002444096)
+  expect_equal(model_result[["c"]], 2.8984079)
   expect_equal(model_result[["d"]], NA_real_)
-  expect_equal(model_result[["alpha"]], 0.14625268)
+  expect_equal(model_result[["alpha"]], 0.345017)
   expect_equal(model_result[["beta"]], NA_real_)
-  expect_equal(model_result[["etrmax_with_photoinhibition"]], 126.78253)
+  expect_equal(model_result[["etrmax_with_photoinhibition"]], 151.851986)
   expect_equal(model_result[["etrmax_without_photoinhibition"]], NA_real_)
-  expect_equal(model_result[["ik_with_photoinhibition"]], 866.87322)
+  expect_equal(model_result[["ik_with_photoinhibition"]], 440.1289906)
   expect_equal(model_result[["ik_without_photoinhibition"]], NA_real_)
-  expect_equal(model_result[["im_with_photoinhibition"]], 1041.89685)
-  expect_equal(model_result[["w"]], -0.79809778)
+  expect_equal(model_result[["im_with_photoinhibition"]], 1399.76963)
+  expect_equal(model_result[["w"]], 1.180362267)
   expect_equal(model_result[["ib"]], NA_real_)
   expect_equal(model_result[["etrmax_with_without_ratio"]], NA_real_)
 })
 
-test_that("test-eilers_peeters_etr_I modified control plot 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-eilers_peeters_etr_I modified control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- eilers_peeters_generate_regression_ETR_I(data)
   model_result <- eilers_peeters_modified(model_result)
@@ -67,7 +67,7 @@ test_that("test-eilers_peeters_etr_I modified control plot 20231122_01.csv", {
       plot_control(
         data,
         model_result,
-        "eilers_peeters ETR I modified 20231122_01.csv",
+        "eilers_peeters ETR I modified 20240925.csv",
         color_eilers_peeters
       )
     )
--- src/tests/testthat/test-eilers_peeters_etr_II.R
+++ src/tests/testthat/test-eilers_peeters_etr_II.R
@@ -1,21 +1,21 @@
-test_that("test-eilers_peeters_etr_II generate regression 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-eilers_peeters_etr_II generate regression 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- eilers_peeters_generate_regression_ETR_II(data)
 
-  expect_equal(model_result[["sdiff"]], 4.0658931)
-  expect_equal(model_result[["a"]], 0.00001294, tolerance = 0.0001)
-  expect_equal(model_result[["b"]], 0.009423141, tolerance = 0.0000001)
-  expect_equal(model_result[["c"]], 10.204545)
-  expect_equal(model_result[["pm"]], 30.862193)
-  expect_equal(model_result[["s"]], 0.09799555)
-  expect_equal(model_result[["ik"]], 314.93464)
-  expect_equal(model_result[["im"]], 888.16408)
-  expect_equal(model_result[["w"]], 0.8201537)
+  expect_equal(model_result[["sdiff"]], 5.7818334)
+  #expect_equal(model_result[["a"]], 0)
+  #expect_equal(model_result[["b"]], 0.005765059)
+  expect_equal(model_result[["c"]], 6.1088971)
+  expect_equal(model_result[["pm"]], 44.5224748)
+  expect_equal(model_result[["s"]], 0.163695670)
+  expect_equal(model_result[["ik"]], 271.98322)
+  expect_equal(model_result[["im"]], 731.801329)
+  expect_equal(model_result[["w"]], 0.690612084)
 })
 
-test_that("test-eilers_peeters_etr_II control plot 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-eilers_peeters_etr_II control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- eilers_peeters_generate_regression_ETR_II(data)
 
@@ -24,40 +24,40 @@ test_that("test-eilers_peeters_etr_II control plot 20231122_01.csv", {
       plot_control(
         data,
         model_result,
-        "eilers_peeters ETR II 20231122_01.csv",
+        "eilers_peeters ETR II 20240925.csv",
         color_eilers_peeters
       )
     )
   )
 })
 
-test_that("test-eilers_peeters_etr_II modified 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-eilers_peeters_etr_II modified 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- eilers_peeters_generate_regression_ETR_II(data)
   expect_no_warning(
     model_result <- eilers_peeters_modified(model_result)
   )
 
-  expect_equal(model_result[["sdiff"]], 4.0658931)
-  expect_equal(model_result[["a"]], 0.00001294, tolerance = 0.0001)
-  expect_equal(model_result[["b"]], 0.009423141, tolerance = 0.0000001)
-  expect_equal(model_result[["c"]], 10.204545)
+  expect_equal(model_result[["sdiff"]], 5.7818334)
+  #expect_equal(model_result[["a"]], 0)
+  #expect_equal(model_result[["b"]], 0.005765059)
+  expect_equal(model_result[["c"]], 6.1088971)
   expect_equal(model_result[["d"]], NA_real_)
-  expect_equal(model_result[["alpha"]], 0.09799555)
+  expect_equal(model_result[["alpha"]], 0.163695670)
   expect_equal(model_result[["beta"]], NA_real_)
-  expect_equal(model_result[["etrmax_with_photoinhibition"]], 30.862193)
+  expect_equal(model_result[["etrmax_with_photoinhibition"]], 44.5224748)
   expect_equal(model_result[["etrmax_without_photoinhibition"]], NA_real_)
-  expect_equal(model_result[["ik_with_photoinhibition"]], 314.93464)
+  expect_equal(model_result[["ik_with_photoinhibition"]], 271.98322)
   expect_equal(model_result[["ik_without_photoinhibition"]], NA_real_)
-  expect_equal(model_result[["im_with_photoinhibition"]], 888.16408)
-  expect_equal(model_result[["w"]], 0.8201537)
+  expect_equal(model_result[["im_with_photoinhibition"]], 731.801329)
+  expect_equal(model_result[["w"]], 0.690612084)
   expect_equal(model_result[["ib"]], NA_real_)
   expect_equal(model_result[["etrmax_with_without_ratio"]], NA_real_)
 })
 
-test_that("test-eilers_peeters_etr_II modified control plot 20231122_01.csv", {
-  test_data_file <- file.path(getwd(), "data", "20231122_01.csv")
+test_that("test-eilers_peeters_etr_II modified control plot 20240925.csv", {
+  test_data_file <- file.path(getwd(), "data", "20240925.csv")
   data <- read_dual_pam_data(test_data_file)
   model_result <- eilers_peeters_generate_regression_ETR_II(data)
   model_result <- model_result <- eilers_peeters_modified(model_result)
@@ -67,7 +67,7 @@ test_that("test-eilers_peeters_etr_II modified control plot 20231122_01.csv", {
       plot <- plot_control(
         data,
         model_result,
-        "eilers_peeters ETR II modified 20231122_01.csv",
+        "eilers_peeters ETR II modified 20240925.csv",
         color_eilers_peeters
       )
     )
