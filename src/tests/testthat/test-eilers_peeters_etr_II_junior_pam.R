925ea2e014fa4ef07366eb448242dba5f7a2a3bf
Phi-S <926151+Phi-S@users.noreply.github.com>
2025-07-29 13:39:05 +0200

    fixed tests for debian

--- src/tests/testthat/test-eilers_peeters_etr_II_junior_pam.R
+++ src/tests/testthat/test-eilers_peeters_etr_II_junior_pam.R
@@ -3,38 +3,30 @@ test_that("test-eilers_peeters_etr_II generate regression junior_pam_20250613.cs
   data <- read_junior_pam_data(test_data_file)
   model_result <- eilers_peeters_generate_regression_ETR_II(data)
 
-  os_name <- Sys.info()[["sysname"]]
+  if (is_supported_os() == FALSE) {
+    skip("Unsupported operating system for this test.")
+  }
 
-  if (os_name == "Linux") {
-    os_release <- readLines("/etc/os-release")
-    distro_line <- grep("^ID_LIKE=", os_release, value = TRUE)
-    distro <- sub("^ID_LIKE=", "", distro_line)
-
-    if (grepl("ubuntu", distro) || grepl("debian", distro)) {
-      expect_equal(model_result[["sdiff"]], 18.5975058)
-      #expect_equal(model_result[["a"]], 0.000001594)
-      expect_equal(model_result[["b"]], 0.01213671)
-      expect_equal(model_result[["c"]], 2.77837046)
-      expect_equal(model_result[["pm"]], 61.1793644)
-      expect_equal(model_result[["s"]], 0.35992320)
-      expect_equal(model_result[["ik"]], 169.978939)
-      expect_equal(model_result[["im"]], 1320.3078)
-      expect_equal(model_result[["w"]], 5.7674788)
-    } else {
-      skip(paste("Skipping test on unsupported Linux distribution:", distro))
-    }
-  } else if (os_name == "Windows") {
-     expect_equal(model_result[["sdiff"]], 18.5975058)
-      #expect_equal(model_result[["a"]], 0.000001594)
-      expect_equal(model_result[["b"]], 0.01213671)
-      expect_equal(model_result[["c"]], 2.77837046)
-      expect_equal(model_result[["pm"]], 61.1793644)
-      expect_equal(model_result[["s"]], 0.35992320)
-      expect_equal(model_result[["ik"]], 169.978939)
-      expect_equal(model_result[["im"]], 1320.3078)
-      expect_equal(model_result[["w"]], 5.7674788)
-  } else {
-    skip(paste("Unsupported operating system:", os_name))
+  if (is_debian_or_ubuntu()) {
+    expect_equal(model_result[["sdiff"]], 18.5975058)
+    # expect_equal(model_result[["a"]], 0.000001594)
+    expect_equal(model_result[["b"]], 0.01213671)
+    expect_equal(model_result[["c"]], 2.77837046)
+    expect_equal(model_result[["pm"]], 61.1793644)
+    expect_equal(model_result[["s"]], 0.35992320)
+    expect_equal(model_result[["ik"]], 169.978939)
+    expect_equal(model_result[["im"]], 1320.3078)
+    expect_equal(model_result[["w"]], 5.7674788)
+  } else if (is_windows()) {
+    expect_equal(model_result[["sdiff"]], 18.5975058)
+    # expect_equal(model_result[["a"]], 0.000001594)
+    expect_equal(model_result[["b"]], 0.01213671)
+    expect_equal(model_result[["c"]], 2.77837046)
+    expect_equal(model_result[["pm"]], 61.1793644)
+    expect_equal(model_result[["s"]], 0.35992320)
+    expect_equal(model_result[["ik"]], 169.978939)
+    expect_equal(model_result[["im"]], 1320.3078)
+    expect_equal(model_result[["w"]], 5.7674788)
   }
 })
 
@@ -47,54 +39,44 @@ test_that("test-eilers_peeters_etr_II junior_pam_20250613.csv.csv", {
     model_result <- eilers_peeters_modified(model_result)
   )
 
-  os_name <- Sys.info()[["sysname"]]
+  if (is_supported_os() == FALSE) {
+    skip("Unsupported operating system for this test.")
+  }
 
-  if (os_name == "Linux") {
-    os_release <- readLines("/etc/os-release")
-    distro_line <- grep("^ID_LIKE=", os_release, value = TRUE)
-    distro <- sub("^ID_LIKE=", "", distro_line)
-
-    if (grepl("ubuntu", distro) || grepl("debian", distro)) {
-      expect_equal(model_result[["sdiff"]], 18.5975058)
-      #expect_equal(model_result[["a"]], 0.000001594)
-      expect_equal(model_result[["b"]], 0.01213671)
-      expect_equal(model_result[["c"]], 2.77837046)
-      expect_equal(model_result[["d"]], NA_real_)
-      expect_equal(model_result[["alpha"]], 0.35992320)
-      expect_equal(model_result[["beta"]], NA_real_)
-      expect_equal(model_result[["etrmax_with_photoinhibition"]], 61.1793644)
-      expect_equal(model_result[["etrmax_without_photoinhibition"]], NA_real_)
-      expect_equal(model_result[["ik_with_photoinhibition"]], 169.978939)
-      expect_equal(model_result[["ik_without_photoinhibition"]], NA_real_)
-      expect_equal(model_result[["im_with_photoinhibition"]], 1320.3078)
-      expect_equal(model_result[["w"]], 5.7674788)
-      expect_equal(model_result[["ib"]], NA_real_)
-      expect_equal(model_result[["etrmax_with_without_ratio"]], NA_real_)
-    } else {
-      skip(paste("Skipping test on unsupported Linux distribution:", distro))
-    }
-  } else if (os_name == "Windows") {
-      expect_equal(model_result[["sdiff"]], 18.5975058)
-      #expect_equal(model_result[["a"]], 0.000001594)
-      expect_equal(model_result[["b"]], 0.01213671)
-      expect_equal(model_result[["c"]], 2.77837046)
-      expect_equal(model_result[["d"]], NA_real_)
-      expect_equal(model_result[["alpha"]], 0.35992320)
-      expect_equal(model_result[["beta"]], NA_real_)
-      expect_equal(model_result[["etrmax_with_photoinhibition"]], 61.1793644)
-      expect_equal(model_result[["etrmax_without_photoinhibition"]], NA_real_)
-      expect_equal(model_result[["ik_with_photoinhibition"]], 169.978939)
-      expect_equal(model_result[["ik_without_photoinhibition"]], NA_real_)
-      expect_equal(model_result[["im_with_photoinhibition"]], 1320.3078)
-      expect_equal(model_result[["w"]], 5.7674788)
-      expect_equal(model_result[["ib"]], NA_real_)
-      expect_equal(model_result[["etrmax_with_without_ratio"]], NA_real_)
-  } else {
-    skip(paste("Unsupported operating system:", os_name))
+  if (is_debian_or_ubuntu()) {
+    expect_equal(model_result[["sdiff"]], 18.5975058)
+    # expect_equal(model_result[["a"]], 0.000001594)
+    expect_equal(model_result[["b"]], 0.01213671)
+    expect_equal(model_result[["c"]], 2.77837046)
+    expect_equal(model_result[["d"]], NA_real_)
+    expect_equal(model_result[["alpha"]], 0.35992320)
+    expect_equal(model_result[["beta"]], NA_real_)
+    expect_equal(model_result[["etrmax_with_photoinhibition"]], 61.1793644)
+    expect_equal(model_result[["etrmax_without_photoinhibition"]], NA_real_)
+    expect_equal(model_result[["ik_with_photoinhibition"]], 169.978939)
+    expect_equal(model_result[["ik_without_photoinhibition"]], NA_real_)
+    expect_equal(model_result[["im_with_photoinhibition"]], 1320.3078)
+    expect_equal(model_result[["w"]], 5.7674788)
+    expect_equal(model_result[["ib"]], NA_real_)
+    expect_equal(model_result[["etrmax_with_without_ratio"]], NA_real_)
+  } else if (is_windows()) {
+    expect_equal(model_result[["sdiff"]], 18.5975058)
+    # expect_equal(model_result[["a"]], 0.000001594)
+    expect_equal(model_result[["b"]], 0.01213671)
+    expect_equal(model_result[["c"]], 2.77837046)
+    expect_equal(model_result[["d"]], NA_real_)
+    expect_equal(model_result[["alpha"]], 0.35992320)
+    expect_equal(model_result[["beta"]], NA_real_)
+    expect_equal(model_result[["etrmax_with_photoinhibition"]], 61.1793644)
+    expect_equal(model_result[["etrmax_without_photoinhibition"]], NA_real_)
+    expect_equal(model_result[["ik_with_photoinhibition"]], 169.978939)
+    expect_equal(model_result[["ik_without_photoinhibition"]], NA_real_)
+    expect_equal(model_result[["im_with_photoinhibition"]], 1320.3078)
+    expect_equal(model_result[["w"]], 5.7674788)
+    expect_equal(model_result[["ib"]], NA_real_)
+    expect_equal(model_result[["etrmax_with_without_ratio"]], NA_real_)
   }
 })
-
-
 
 test_that("test-eilers_peeters_etr_II modified control plot junior_pam_20250613.csv", {
   test_data_file <- file.path(getwd(), "data_junior_pam", "junior_pam_20250613.csv")
