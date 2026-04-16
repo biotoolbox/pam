a9fad9aebe839c513d76bf2c6132ffafe83eaad8
JulienBoehm <180776296+JulienBoehm@users.noreply.github.com>
2026-04-16 16:40:20 +0200

    Add read_dual_pam_data_single_channel_II function, and tests

--- src/R/validation.R
+++ src/R/validation.R
@@ -100,6 +100,52 @@ validate_dual_pam_data <- function(data) {
   }
 }
 
+validate_dual_pam_data_single_channel_II <- function(data) {
+  if (is.null(data)) {
+    stop("data is null")
+  }
+
+  if (!data.table::is.data.table(data)) {
+    stop("data is not a valid data.table")
+  }
+
+  if (nrow(data) < 2) {
+    stop("no data rows")
+  }
+
+  if (ncol(data) == 0) {
+    stop("no cols in data")
+  }
+
+  if (!"ID" %in% colnames(data)) {
+    stop("required col 'ID' not found")
+  }
+
+  if (!"PAR" %in% colnames(data)) {
+    stop("required col 'PAR' not found")
+  }
+
+  if (!"Y.II." %in% colnames(data)) {
+    stop("required col 'Y(II)' not found")
+  }
+
+  if (!"Action" %in% colnames(data)) {
+    stop("required col 'Action' not found")
+  }
+
+  if (!"Date" %in% colnames(data)) {
+    stop("required col 'Date' not found")
+  }
+
+  if (!"Time" %in% colnames(data)) {
+    stop("required col 'Time' not found")
+  }
+
+  if (!"Fm-Det." %in% data[["Action"]]) {
+    stop("required value 'Fm' not found in column 'Action'")
+  }
+}
+
 validate_junior_pam_data <- function(data) {
   if (is.null(data)) {
     stop("data is null")
